676d0a749cc16e8d303ac4523c7504511966c6a3
gonzalo lavin <gonzalolavin99@gmail.com>
2023-11-01 22:17:00 -0300

    Lista de colaboradores  agregada y tipo de tabla agregado

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import Table from 'react-bootstrap/Table';
+
 
 const Listado = () => {
   return (
-    <div>Listado</div>
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Nombre</th>
+          <th>Correo</th>
+          <th>Edad</th>
+          <th>Cargo</th>
+          <th>Teléfono</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </Table>
   )
 }
 
